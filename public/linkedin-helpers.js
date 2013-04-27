28c0eabe1fa30ee7105efc0e8beef50610540fed
bigthyme <asinghavong@gmail.com>
2013-04-27 10:49:08 -0700

    Pivoting to resumes...

--- public/linkedin-helpers.js
+++ public/linkedin-helpers.js
@@ -1,43 +1,37 @@
 // Linkedin API Helper functions
 var profileSearchAttributes = [
       "id","firstName","lastName","industry","headline",
-      "location","distance","currentShare","network",
-      "summary","specialties","interests","positions",
-      "skills","educations","phoneNumbers","imAccounts",
-      "primaryTwitterAccount","date-of-birth","main-address"
+      "location","distance", "summary","specialties","interests","positions","skills","educations","phoneNumbers","imAccounts","primaryTwitterAccount","date-of-birth","main-address"
 ];
-
-var expertSearchCritera = [
-  "id","firstName","lastName","industry","headline",
-      "location", "specialties", "distance"
-];
-
 
 var onLinkedInLoad = function() {
   IN.Event.on(IN, "auth", function(){
     //pass 'me' as auth token
-    IN.API.PeopleSearch()
-      .fields(expertSearchCritera)
-      .params({"count": 5, "distance" : 1})
-      .result(showSearchResults);
+    IN.API.Profile("me")
+      .fields(profileSearchAttributes)
+      .result(getInfo);
   })
 }
 
-var showSearchResults = function(profile) {
-  if(profile){
-    var experts = profile.people.values;
-    console.log(experts);
-    for(var i = 0; i < experts.length; i++){
-      addExpert(experts[i]);
-      // var validateExpert = Experts.find({firstName: experts[i].firstName, lastName: experts[i].lastName});
-      //   (validateExpert.count < 1) ? addExpert(experts[i]) : console.log(experts[i].firstName + ' already exists');
-    }
+//Gets profile information
+var getInfo = function(profile) {
+  var linkedinInfo = profile.values[0]
+
+  //find logged in user's unique indentifiers in the db
+  var preventDuplicates = Experts.find({firstName: linkedinInfo.firstName, lastName: linkedinInfo.lastName, distance: 0});
+  console.log(preventDuplicates.count());
+  if (preventDuplicates.count() < 1) {
+    addInfo(linkedinInfo);
+  } else {
+    console.log(linkedinInfo.firstName + ' ' + linkedinInfo.lastName + ' has been updated!');
   }
 }
 
-var addExpert = function(profile) {
-  if(profile){
-    Experts.insert(profile);
-  }
+//Adds validated information to a database
+var addInfo = function(profile) {
+  Experts.insert({distance: profile.distance, firstName: profile.firstName, lastName: profile.lastName, });
 }
 
+var updateInfo = function(profile) {
+  Experts.update({distance: profile.distance, firstName: profile.firstName, lastName: profile.lastName}, profile);
+}
