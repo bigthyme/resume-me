4d2e4918003a4b78f06fe0f80c0292b7dd4e1fd2
bigthyme <asinghavong@gmail.com>
2013-04-28 10:37:09 -0700

    Fixed WYSIWYG w/closure

--- client/linkedIn.js
+++ client/linkedIn.js
@@ -1,19 +1,33 @@
 Meteor.subscribe("resume-data");
 Meteor.subscribe("resume-html");
 
-Template.profiles.allProfiles = function(){
+Template.resumeBody.allResumes = function(){
   console.log(resumeData.find({}));
   return resumeData.find({});
-}
+};
 
 Template.main.currentUser = function(){
   console.log(Session.get('currentUserId'));
   return Session.get('currentUserId');
 };
 
-Template.linkedinInfo.editableContent = function(){
-  $('.resume-body').wysiwyg();
-  $('.save-html').on('click', function(){
-    console.log('hello');
-  });
+Template.welcome.userName = function(){
+  return Session.get('currentUserName');
 }
+
+Template.linkedinInfo.editableContent = function(){
+  //TODO: Need to modularize this!
+    //TODO: why do we need the closure?
+ $(function(){
+    $('.header-info').wysiwyg();
+    $('.skills').wysiwyg();
+    $('.education').wysiwyg();
+    $('.experience').wysiwyg();
+  })
+};
+
+Template.linkedinInfo.events({
+  "click span" : function(e){
+    console.log('test');
+  }
+});
