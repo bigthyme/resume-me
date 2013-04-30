67f4101013e76ff1d2f242835b566cfcc7bddfa3
bigthyme <asinghavong@gmail.com>
2013-04-29 17:39:18 -0700

    Updated skill-list array

--- public/wysiwyg-helpers.js
+++ public/wysiwyg-helpers.js
@@ -6,20 +6,42 @@ $(function(){
 
   //save data
   $('body').on('click', '#send-btn', function(){
-    var resume = {};
+    var resume = {},
+    skillsList = [];
 
+    //TODO: Use recursion??
     $('span').each(function(i){
-      // if($('span')[i].className === "firstname"){
-      //   resume.firstname = $(this).text();
-      // };
-      if(resume[$('span')[i].className]){
-        console.log('matching key!');
-        var preConcat = resume[$('span')[i].className];
+     if($('span')[i].className === "firstname"){
+        var firstname = $(this).text();
+        resume["name"] = firstname;
       }
-
-      resume[$('span')[i].className] = $(this).text();
-      console.log(resume);
+      if($('span')[i].className === "lastname"){
+        var lastname = $(this).text();
+        var first = resume["name"];
+        resume["name"] = first + ' ' + lastname;
+        console.log(resume["name"]);
+      }
+      if($('span')[i].className === "email-address"){
+        var email = $(this).text();
+        resume["email"] = email;
+        console.log(resume);
+      }
+      if($('span')[i].className === "phone-number"){
+        var phoneNumber = $(this).text();
+        resume["phoneNumber"] = phoneNumber;
+      }
+      if($('span')[i].className === "location"){
+        var location = $(this).text();
+        resume["location"] = location;
+        console.log(resume);
+      };
+      if($('span')[i].className === "skill"){
+        var skill = $(this).text();
+        skillsList.push(skill);
+        console.log(skillsList);
+      }
     });
+
     // for( var i = 0; i < $('span').length - 1; i++) {
     //   if($('span')[i].attr('class') === "firstname"){
     //     $('span')[i].innerText;
@@ -47,19 +69,19 @@ $(function(){
     //   updateHtml(saveData);
     //   // Session.set('dataReady', saveData);
     // }
-  });
-
-  var addHtml = function(data) {
-    //TODO: using linkedin id, need to use mongo_id.
-    resumeHtml.insert({ headerData: data[0], skillsData: data[1], educationData: data[2], experienceData: data[3], linked_id: idLookUp });
-    console.log('adding!');
-  }
-
-  var updateHtml = function(data) {
-    resumeHtml.update({ _id : this._id }, {headerData: data[0], skillsData: data[1], educationData: data[2], experienceData: data[3], linked_id: idLookUp });
-    console.log('updating!');
-  }
+  })
 });
+//   var addHtml = function(data) {
+//     //TODO: using linkedin id, need to use mongo_id.
+//     resumeHtml.insert({ headerData: data[0], skillsData: data[1], educationData: data[2], experienceData: data[3], linked_id: idLookUp });
+//     console.log('adding!');
+//   }
+
+//   var updateHtml = function(data) {
+//     resumeHtml.update({ _id : this._id }, {headerData: data[0], skillsData: data[1], educationData: data[2], experienceData: data[3], linked_id: idLookUp });
+//     console.log('updating!');
+//   }
+// });
 
 // Valiation Functions
 var validateStrings = function(dataArray) {
